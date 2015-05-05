b273a44914501d1d0524dc7638dbb3aeb46dc607
minhhoangtcu <minh.hoang@tcu.edu>
2015-05-04 22:26:08 -0500

    Added the Fact book project. Will do if I have time.

---
+++ src/networking/FactBook.java
@@ -0,0 +1,9 @@
+/*
+ * Write a program that allows a user to query the CIA World Fact Book
+ * for facts about a country, such as the size, average income, capital city, and so on. 
+ */
+package networking;
+
+public class FactBook {
+
+}
