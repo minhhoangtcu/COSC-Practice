15ca8ce07571c2500886541353214cead929b3c0
Minh Hoang <minh.hoang@tcu.edu>
2015-07-23 22:34:09 +0700

    Implemented a stack

---
+++ src/stacksAndQueuesPractice/Stack.java
@@ -0,0 +1,41 @@
+package stacksAndQueuesPractice;
+
+import java.util.Arrays;
+
+public class Stack {
+	
+	int[] stack;
+	int topOfStack = -1;
+	int stackSize;
+	
+	public Stack(int stackSize) {
+		this.stackSize = stackSize;
+		stack = new int[stackSize];
+		Arrays.fill(stack, -1); // -1 will represent as empty. We can change this representation later.
+	}
+	
+	public void push(int number) {
+		if (topOfStack + 1 < stackSize) { // Check to see if we are trying to push out of the limit of the stack or not
+			System.out.printf("Pushed number %d into the top of stack(%d)", number, topOfStack);
+			topOfStack++;
+			stack[topOfStack] = number;
+		}
+		else {
+			System.out.println("The stack is fulled. Unable to push to the top of stack.");
+		}
+	}
+	
+	public int pop() {
+		if (topOfStack >= 0) {
+			int output = stack[topOfStack];
+			stack[topOfStack] = -1; // Again, -1 will represent as empty.
+			topOfStack--;
+			return output;
+		}
+		else {
+			System.out.println("The stack is empty. Unable to pop at the top of stack.");
+			return -1;
+		}
+	}
+
+}
