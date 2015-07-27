563753e9db70d64c264cbe45587d21ff30ab406c
Minh Hoang <minh.hoang@tcu.edu>
2015-07-27 22:01:21 +0700

    Tested the insert last node method. Worked as expected.

--- src/linkListPractice/DoublyLinkedListTest.java
+++ src/linkListPractice/DoublyLinkedListTest.java
@@ -15,6 +15,7 @@ public class DoublyLinkedListTest {
 		list.insertFirstNode("Don Quixote", 315000000, 1605);
 		list.insertFirstNode("A Tale Of Two Cities", 200000000, 1859);
 		list.insertFirstNode("The Lord of the Rings", 150000000, 1954);
+		list.insertLastNode("A Game of Ice and Fire", 50000000, 1995);
 		list.insertFirstNode("The Hobbit", 140600000, 1937);
 	}
 }
