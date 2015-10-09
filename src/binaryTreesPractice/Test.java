90b58b4fd275433dd49147b93352eeaba02bc626
minhhoangtcu <minh.hoang@tcu.edu>
2015-10-09 16:47:13 -0500

    Tested in order traverse.

---
+++ src/binaryTreesPractice/Test.java
@@ -0,0 +1,24 @@
+package binaryTreesPractice;
+
+public class Test {
+
+	@org.junit.Test
+	public void inOrderTraverse() throws InterruptedException {
+		Tree tree = initializeTree();
+		inOrderTraverseTree.printAllNodes(tree);
+		Thread.sleep(60000);
+	}
+	
+	private static Tree initializeTree() {
+		Tree tree = new Tree();
+		tree.addNode(146340, "Dentist");
+		tree.addNode(90000, "Nurse Practitioner");
+		tree.addNode(100000, "Software Developer");
+		tree.addNode(123300, "Physician");
+		tree.addNode(71110, "Dental Hygienist");
+		tree.addNode(80000, "Physical Therapist");
+		tree.addNode(70000, "Computer Systems Analyst");
+		return tree;
+	}
+
+}
---
+++ src/binaryTreesPractice/inOrderTraverseTree.java
@@ -0,0 +1,18 @@
+package binaryTreesPractice;
+
+public class inOrderTraverseTree {
+	
+	public static void printAllNodes (Tree tree) {
+		Node focusNode = tree.root;
+		traverseRecursively(focusNode);
+	}
+	
+	private static void traverseRecursively(Node focusNode) {
+		if (focusNode != null) {
+			traverseRecursively(focusNode.leftNode);
+			System.out.println(focusNode.toString());
+			traverseRecursively(focusNode.rightNode);
+		}
+	}
+	
+}
