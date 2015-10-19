8feeffed07c412fb749d03224f247400326e52dc
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-10-19 00:53:24 -0500

    Finished and tested the stack with auto resize

---
+++ src/datastructure/stacksAndQueuesPractice/FixedAutoResizeStack.java
@@ -0,0 +1,67 @@
+package datastructure.stacksAndQueuesPractice;
+
+/*
+ * This is a stack using fixed-capacity (an array)
+ * The size of this stack will automatically resize. 
+ */
+public class FixedAutoResizeStack<E> extends FixedStack<E>{
+	
+	public static void main(String[] args) {
+		FixedAutoResizeStack<String> stack = new FixedAutoResizeStack<>();
+		
+		stack.push("To");
+		stack.push("Be");
+		stack.push("Or");
+		stack.push("Not");
+		stack.push("To");
+		stack.push("Be");
+		stack.push("To");
+		stack.push("Be");
+		stack.push("Or");
+		stack.push("Not");
+		stack.push("To");
+		stack.push("Be");
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+	}
+	
+	public FixedAutoResizeStack() {
+		this(5);
+	}
+	
+	public FixedAutoResizeStack(int size) {
+		elements = (E[]) new Object[size];
+	}
+	
+	public void push(E item) {
+		if (N == elements.length)
+			elements = resize(2*elements.length);
+		elements[N++] = item;
+	}
+	
+	public E pop() {
+		E output = elements[--N];
+		elements[N] = null;
+		if (N > 0 && N == elements.length/4)
+			elements = resize(elements.length/2);
+		return output;
+	}
+	
+	private E[] resize(int newSize) {
+		E[] temp = (E[]) new Object[newSize];
+		for (int i = 0; i < N; i++) {
+			temp[i] = elements[i];
+		}
+		return temp;
+	}
+}
