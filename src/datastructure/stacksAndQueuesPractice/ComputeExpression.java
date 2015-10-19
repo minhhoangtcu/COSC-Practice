109437f53fa077649138ae8c0a61903ad03812a4
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-10-19 02:06:00 -0500

    Fixed/Tested the program

--- src/datastructure/stacksAndQueuesPractice/ComputeExpression.java
+++ src/datastructure/stacksAndQueuesPractice/ComputeExpression.java
@@ -6,7 +6,23 @@ public class ComputeExpression {
 	
 	public static void main(String[] args) {
 		ComputeExpression computing = new ComputeExpression();
-		System.out.println(computing.compute("(((5 + 3) * 4) + (5 / 3))"));
+		String result = String.format("%.2f", computing.compute("(((5 + 3) * 4) + (5 / 3))"));
+		System.out.println(result);
+		
+		result = String.format("%.2f", computing.compute("(1 + 1)"));
+		System.out.println(result);
+		
+		result = String.format("%.2f", computing.compute("(2 * 5)"));
+		System.out.println(result);
+		
+		result = String.format("%.2f", computing.compute("(7 / 3)"));
+		System.out.println(result);
+		
+		result = String.format("%.2f", computing.compute("((((5 + 3) * 4) + (5 / 3)) + (1 + 3))"));
+		System.out.println(result);
+		
+		result = String.format("%.2f", computing.compute("(((5 + (3 - 3)) * 4) + (5 / 3))"));
+		System.out.println(result);
 	}
 	
 	/*
@@ -14,31 +30,32 @@ public class ComputeExpression {
 	 * @param a String expression of the form: (((5 + 3) * 4) + (5 / 3))
 	 * @output a result of the expression.
 	 */
-	public int compute(String text) {
-		FixedAutoResizeStack<Integer> stackNum = new FixedAutoResizeStack<>();
+	public float compute(String text) {
+		FixedAutoResizeStack<Float> stackNum = new FixedAutoResizeStack<>();
 		FixedAutoResizeStack<String> stackOper = new FixedAutoResizeStack<>();
 		
 		for (String character: text.split("")) {
-			if (isInteger(character)) stackNum.push(Integer.parseInt(character));
+			if (isFloat(character)) stackNum.push(Float.parseFloat(character));
 			else if (isOperator(character)) stackOper.push(character);
 			else if (character.equals(")")) {
-				int numOne = stackNum.pop();
-				int numTwo = stackNum.pop();
+				float numOne = stackNum.pop();
+				float numTwo = stackNum.pop();
 				String operator = stackOper.pop();
 				switch (operator) {
 				case "+":
-					stackNum.push(numOne + numTwo);
+					stackNum.push(numTwo + numOne);
 					break;
 				case "-":
-					stackNum.push(numOne - numTwo);
+					stackNum.push(numTwo - numOne);
 					break;
 				case "/":
-					stackNum.push(numOne / numTwo);
+					stackNum.push(numTwo / numOne);
 					break;
 				case "*":
 					stackNum.push(numOne * numTwo);
 					break;
 				}
+				System.out.println(String.format("%.2f %s %.2f", numTwo, operator, numOne));
 			}
 		}
 		return stackNum.pop();
@@ -55,6 +72,16 @@ public class ComputeExpression {
 		return false;
 	}
 
+	private boolean isFloat(String character) {
+		try {
+			Float.parseFloat(character);
+			return true;
+		}
+		catch (Exception e) {
+			return false;
+		}
+	}
+	
 	private boolean isInteger(String character) {
 		try {
 			Integer.parseInt(character);
