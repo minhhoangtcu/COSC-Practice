0225bce79e1b79c2989be2d5fd80a9f65adac0bd
minhhoangtcu <minh.hoang@tcu.edu>
2015-11-16 22:43:51 -0600

    Tree can now find predecessor

--- src/datastructure/trees/binarysearchtree/Tree.java
+++ src/datastructure/trees/binarysearchtree/Tree.java
@@ -38,6 +38,38 @@ public class Tree<T extends Comparable<T>> {
 		}
 	}
 	
+	public T predecessor(T data) {
+		Node<T> x = predecessor(root, data);
+		if (x == null)
+			return null;
+		else
+			return x.data;
+	}
+	
+	private Node<T> predecessor(Node<T> node, T data) {
+		if (node == null | isEmpty())
+			return null;
+		else {
+			int comp = data.compareTo(node.data);
+			if (comp == 0) // Found a node similar that has the same key with the provided key, so this must be the predecessor
+				return node;
+			else if (comp < 0)
+				return predecessor(node.left, data); // If the node key is less than the key, then the predecessor must be on the left
+			else {
+				Node<T> right = predecessor(node.right, data); // If the node key is more than the key, the the predecessor may be on the right. 
+															   // If there is one, return it. If not, return the parent.
+				if (right != null) 
+					return right;
+				else
+					return node;
+			}
+		}
+	}
+	
+	public Node successor() {
+		return null;
+	}
+	
 	public void push(T[] input) {
 		for (T each: input) {
 			push(each);
--- src/datastructure/trees/binarysearchtree/TreeTest.java
+++ src/datastructure/trees/binarysearchtree/TreeTest.java
@@ -6,8 +6,12 @@ public class TreeTest {
 		Tree<String> tree = new Tree<>();
 		Traversal<String> traversal = new Traversal<>();
 		
-		String[] input = {"a", "c", "d", "b", "e"};
+		String[] input = {"a", "c", "g", "b", "e", "h", "r", "u"};
 		tree.push(input);
+		System.out.println("Pred of C: " + tree.predecessor("i"));
+		System.out.println("Pred of D: " + tree.predecessor("d"));
+		System.out.println("Pred of F: " + tree.predecessor("f"));
+		
 		System.out.println("Print In-Order");
 		traversal.printInOrderRecursively(tree.root);
 		System.out.println("\nPrint Pre-Order");
